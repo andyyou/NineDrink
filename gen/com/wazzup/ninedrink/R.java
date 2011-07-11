67ba3e509feb991c02097c4288a7c7b9bd6dce86
Eddy Yang <yz3210@gmail.com>
2011-07-11 15:37:13 +0800

    Rename Mainmenu.java to Settings.java

--- gen/com/wazzup/ninedrink/R.java
+++ gen/com/wazzup/ninedrink/R.java
@@ -83,7 +83,7 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int splash=0x7f030001;
+        public static final int settings=0x7f030001;
     }
     public static final class string {
         public static final int about_msg=0x7f050005;
