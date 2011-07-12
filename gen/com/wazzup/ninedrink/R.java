254c915b8f0113cbfccebc7dfca52c43987f5f01
Eddy Yang <yz3210@gmail.com>
2011-07-12 11:21:32 +0800

    Delete bin/ and add sound file.

--- gen/com/wazzup/ninedrink/R.java
+++ gen/com/wazzup/ninedrink/R.java
@@ -57,44 +57,47 @@ public final class R {
         public static final int selector_start=0x7f020026;
     }
     public static final class id {
-        public static final int blank=0x7f060014;
-        public static final int btn_reset=0x7f060003;
-        public static final int btn_setcancel=0x7f060016;
-        public static final int btn_setcomplete=0x7f060015;
-        public static final int checkBox0=0x7f060013;
-        public static final int checkBox1=0x7f060006;
-        public static final int checkBox10=0x7f06000f;
-        public static final int checkBox11=0x7f060010;
-        public static final int checkBox12=0x7f060011;
-        public static final int checkBox13=0x7f060012;
-        public static final int checkBox2=0x7f060007;
-        public static final int checkBox3=0x7f060008;
-        public static final int checkBox4=0x7f060009;
-        public static final int checkBox5=0x7f06000a;
-        public static final int checkBox6=0x7f06000b;
-        public static final int checkBox7=0x7f06000c;
-        public static final int checkBox8=0x7f06000d;
-        public static final int checkBox9=0x7f06000e;
-        public static final int checkBoxAll=0x7f060005;
-        public static final int imgbtn_1=0x7f060001;
-        public static final int imgbtn_close=0x7f060004;
-        public static final int imgbtn_set=0x7f060002;
-        public static final int show_msg=0x7f060000;
+        public static final int blank=0x7f070014;
+        public static final int btn_reset=0x7f070003;
+        public static final int btn_setcancel=0x7f070016;
+        public static final int btn_setcomplete=0x7f070015;
+        public static final int checkBox0=0x7f070013;
+        public static final int checkBox1=0x7f070006;
+        public static final int checkBox10=0x7f07000f;
+        public static final int checkBox11=0x7f070010;
+        public static final int checkBox12=0x7f070011;
+        public static final int checkBox13=0x7f070012;
+        public static final int checkBox2=0x7f070007;
+        public static final int checkBox3=0x7f070008;
+        public static final int checkBox4=0x7f070009;
+        public static final int checkBox5=0x7f07000a;
+        public static final int checkBox6=0x7f07000b;
+        public static final int checkBox7=0x7f07000c;
+        public static final int checkBox8=0x7f07000d;
+        public static final int checkBox9=0x7f07000e;
+        public static final int checkBoxAll=0x7f070005;
+        public static final int imgbtn_1=0x7f070001;
+        public static final int imgbtn_close=0x7f070004;
+        public static final int imgbtn_set=0x7f070002;
+        public static final int show_msg=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int settings=0x7f030001;
     }
+    public static final class raw {
+        public static final int deal1=0x7f050000;
+    }
     public static final class string {
-        public static final int about_msg=0x7f050009;
-        public static final int about_title=0x7f050008;
-        public static final int app_name=0x7f050005;
-        public static final int btn_reset=0x7f050000;
-        public static final int cancel=0x7f050004;
-        public static final int done=0x7f050003;
-        public static final int lb_homepage=0x7f050006;
-        public static final int select_all=0x7f050002;
-        public static final int setting_title=0x7f050001;
-        public static final int url_homepage=0x7f050007;
+        public static final int about_msg=0x7f060009;
+        public static final int about_title=0x7f060008;
+        public static final int app_name=0x7f060005;
+        public static final int btn_reset=0x7f060000;
+        public static final int cancel=0x7f060004;
+        public static final int done=0x7f060003;
+        public static final int lb_homepage=0x7f060006;
+        public static final int select_all=0x7f060002;
+        public static final int setting_title=0x7f060001;
+        public static final int url_homepage=0x7f060007;
     }
 }
